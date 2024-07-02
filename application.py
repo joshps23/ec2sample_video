4919f338ecd445b29fd9df89c1fd1b6a95190423
joshps23 <huixmun@gmail.com>
2024-07-02 22:47:35 +0800

    added webcam

--- application.py
+++ application.py
@@ -68,6 +68,11 @@ def video():
     #return Response(generate_frames(path_x='static/files/bikes.mp4'), mimetype='multipart/x-mixed-replace; boundary=frame')
     return Response(generate_frames(path_x = session.get('video_path', None)),mimetype='multipart/x-mixed-replace; boundary=frame')
 
+# To display the Output Video on Webcam page
+@app.route('/webapp')
+def webapp():
+    #return Response(generate_frames(path_x = session.get('video_path', None),conf_=round(float(session.get('conf_', None))/100,2)),mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames_web(path_x=0), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0",port=5000)
