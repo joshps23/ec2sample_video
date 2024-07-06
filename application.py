44adff5e6c10c358b87080861434fd75b2c02f94
joshps23 <huixmun@gmail.com>
2024-07-06 12:46:24 +0800

    added frontend video

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from flask_wtf import FlaskForm
 from werkzeug.utils import secure_filename
 from wtforms.validators import InputRequired,NumberRange
 import os
-
+import base64
 import cv2
 
 
@@ -42,11 +42,62 @@ def generate_frames_web(path_x):
                     b'Content-Type: image/jpeg\r\n\r\n' + frame +b'\r\n')
 
 @app.route('/')
+# def hello_world():
+#   session.clear()
+#   random_number = random.randint(1, 100)
+#   current_year = datetime.datetime.now().year
+#   return render_template("index.html", num=random_number, yr=current_year)
+
+@app.route('/')
 def hello_world():
-  session.clear()
-  random_number = random.randint(1, 100)
-  current_year = datetime.datetime.now().year
-  return render_template("index.html", num=random_number, yr=current_year)
+  return render_template("indexvideo.html")
+#     return """
+#     <!DOCTYPE html>
+#     <html lang="en">
+#     <head>
+#         <meta charset="UTF-8">
+#         <title>Title</title>
+#     </head>
+#     <body>
+#     <video id="video" width="640" height="480" autoplay></video>
+#     <button id="snap">Snap Photo</button>
+#     <canvas id="canvas" width="640" height="480"></canvas>
+#     </body>
+#     <script>
+
+#     var video = document.getElementById('video');
+#     if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+#         navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+#             //video.src = window.URL.createObjectURL(stream);
+#             video.srcObject = stream;
+#             video.play();
+#         });
+#     }
+
+#     var canvas = document.getElementById('canvas');
+#     var context = canvas.getContext('2d');
+#     var video = document.getElementById('video');
+
+#     // Trigger photo take
+#     document.getElementById("snap").addEventListener("click", function() {
+#         context.drawImage(video, 0, 0, 640, 480);
+#     var request = new XMLHttpRequest();
+#     request.open('POST', '/submit?image=' + video.toString('base64'), true);
+#     request.send();
+#     });
+
+
+
+# </script>
+# </html>
+#     """
+
+@app.route('/submit',methods=['POST'])
+def submit():
+    image = request.args.get('image')
+
+    print(type(image))
+    return ""
 
 @app.route('/anotherpage')
 def get_another_page():
